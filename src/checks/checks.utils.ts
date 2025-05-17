fa96db318da18f7c827d14bed8701d5825b32664
Ankan Poddar <apoddar1701@gmail.com>
2025-05-18 02:05:53 +0530

    Fix lint

--- src/checks/checks.utils.ts
+++ src/checks/checks.utils.ts
@@ -1,16 +1,18 @@
-import { InternalServerErrorException } from "../common/errorHandler";
+import { InternalServerErrorException } from '../common/errorHandler';
 
 export const getProjectsInOrg = async (organizationId: string, accessToken: string) => {
   const response = await fetch(`https://api.supabase.com/v1/projects`, {
     headers: {
-      'Authorization': `Bearer ${accessToken}`
-    }
+      Authorization: `Bearer ${accessToken}`,
+    },
   });
   if (!response.ok) {
     console.log(response);
     throw new InternalServerErrorException('Failed to fetch projects');
   }
   const projects = await response.json();
-  const projectsInCurrentOrg = projects.filter((project: any) => project.organization_id === organizationId);
+  const projectsInCurrentOrg = projects.filter(
+    (project: { organization_id: string }) => project.organization_id === organizationId,
+  );
   return projectsInCurrentOrg;
-}
+};
